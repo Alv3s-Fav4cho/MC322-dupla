eb4a9cfe5cf60cfd1d810a32601eb5a332f4c12f
Daniel Alves <102082350+Alv3sDaniel@users.noreply.github.com>
2022-05-10 21:34:01 -0300

    criação do projeto do lab05
    criação do projeto do lab05 junto com algumas implementaçoes

---
+++ Lab05/lab05/src/pt/c40task/l05wumpus/Fedor.java
@@ -0,0 +1,8 @@
+package pt.c40task.l05wumpus;
+
+public class Fedor extends Wumpus{
+
+	public Fedor(String situacao) {
+		super(situacao);
+	}
+}
---
+++ Lab05/lab05/src/pt/c40task/l05wumpus/Sala.java
@@ -0,0 +1,18 @@
+package pt.c40task.l05wumpus;
+
+public class Sala {
+
+	private String situacao;
+	
+	public Sala(String situacao){
+		this.situacao = situacao;
+	}
+	
+	public String getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+}
