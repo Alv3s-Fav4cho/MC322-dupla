b1099c263894a9b51dccb654b8832d85a9e286e3
Daniel Alves <dani.rochapq@gmail.com>
2022-06-08 16:39:18 -0300

    cria logica de geracao aleatoria

--- trabalho/src/pacote/AppLost.java
+++ trabalho/src/pacote/AppLost.java
@@ -3,7 +3,15 @@ package pacote;
 public class AppLost {
 
 	public static void main(String[] args) {
+		
+		Montador m = new Montador();
 		System.out.println("Hello world!");
+		
+		/*
+		 * System.out.println(5 / 3); System.out.println((5 % 3));
+		 */
+		 
+		m.montar();
 		
 	}
 }
--- trabalho/src/pacote/Montador.java
+++ trabalho/src/pacote/Montador.java
@@ -1,10 +1,66 @@
 package pacote;
 
+import java.util.Random;
+
 public class Montador {
 	Nether neth;
 	
 	public void montar() {
+		int x;
+		int linha = 0;
+		int coluna = 0; 
+		//matriz 5x5, ordem n=5
+		int n = 5;
+		String classe = "teste";
+		Random rand = new Random();
 		
+		for(int i = 0; i<=2; i++ ) {
+			if (i == 0) {
+				classe = "Protagonista";
+				x = rand.nextInt(26) + 1;
+				if (x % n == 0) {
+					linha = x/n - 1;
+					coluna = n - 1;
+				} else if(x/n == 0){
+					linha = x/n;
+					coluna = x%n - 1;
+				}
+				else{
+					linha = x/n;
+					coluna = x%n - 1;
+				}
+			} else if (i == 1) {
+				classe = "PessoaPerdida";
+				x = rand.nextInt(26) + 1;
+				if (x % n == 0) {
+					linha = x/n - 1;
+					coluna = n - 1;
+				} else if(x/n == 0){
+					linha = x/n;
+					coluna = x%n - 1;
+				}
+				else{
+					linha = x/n;
+					coluna = x%n - 1;
+				}
+			} else {
+				classe = "Porta";
+				x = rand.nextInt(26) + 1;
+				if (x % n == 0) {
+					linha = x/n - 1;
+					coluna = n - 1;
+				} else if(x/n == 0){
+					linha = x/n;
+					coluna = x%n - 1;
+				}
+				else{
+					linha = x/n;
+					coluna = x%n - 1;
+				}
+			}
+			System.out.println("numero aleatorio gerado: "+ x);
+			System.out.println("posicao correspondente do "+classe+" na matriz: ["+linha+"]["+coluna+"]");				
+		}
 	}
 	
 	public void conectaNether(Nether neth) {
