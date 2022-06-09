1227f4fd81aeea846e06591479ca94a6db9d7638
ra247024 <ra247024@kraft.lab.ic.unicamp.br>
2022-06-09 14:38:08 -0300

    altera range do numero aleatorio

--- trabalho/src/pacote/Montador.java
+++ trabalho/src/pacote/Montador.java
@@ -4,65 +4,67 @@ import java.util.Random;
 
 public class Montador {
 	Nether neth;
-	
+
 	public void montar() {
 		int x;
 		int linha = 0;
-		int coluna = 0; 
-		//matriz 5x5, ordem n=5
+		int coluna = 0;
+		// matriz 5x5, ordem n=5
 		int n = 5;
 		String classe = "teste";
 		Random rand = new Random();
-		
-		for(int i = 0; i<=2; i++ ) {
+
+		for (int i = 0; i <= 2; i++) {
+			/*
+			 * if (i % n == 0) { linha = i/n - 1; coluna = n - 1; } else if(i/n == 0){ linha
+			 * = i/n; coluna = i%n - 1; } else{ linha = i/n; coluna = i%n - 1; }
+			 */
+
 			if (i == 0) {
 				classe = "Protagonista";
-				x = rand.nextInt(26) + 1;
+				x = rand.nextInt(25) + 1;
 				if (x % n == 0) {
-					linha = x/n - 1;
+					linha = x / n - 1;
 					coluna = n - 1;
-				} else if(x/n == 0){
-					linha = x/n;
-					coluna = x%n - 1;
-				}
-				else{
-					linha = x/n;
-					coluna = x%n - 1;
+				} else if (x / n == 0) {
+					linha = x / n;
+					coluna = x % n - 1;
+				} else {
+					linha = x / n;
+					coluna = x % n - 1;
 				}
 			} else if (i == 1) {
 				classe = "PessoaPerdida";
-				x = rand.nextInt(26) + 1;
+				x = rand.nextInt(25) + 1;
 				if (x % n == 0) {
-					linha = x/n - 1;
+					linha = x / n - 1;
 					coluna = n - 1;
-				} else if(x/n == 0){
-					linha = x/n;
-					coluna = x%n - 1;
-				}
-				else{
-					linha = x/n;
-					coluna = x%n - 1;
+				} else if (x / n == 0) {
+					linha = x / n;
+					coluna = x % n - 1;
+				} else {
+					linha = x / n;
+					coluna = x % n - 1;
 				}
 			} else {
 				classe = "Porta";
-				x = rand.nextInt(26) + 1;
+				x = rand.nextInt(25) + 1;
 				if (x % n == 0) {
-					linha = x/n - 1;
+					linha = x / n - 1;
 					coluna = n - 1;
-				} else if(x/n == 0){
-					linha = x/n;
-					coluna = x%n - 1;
-				}
-				else{
-					linha = x/n;
-					coluna = x%n - 1;
+				} else if (x / n == 0) {
+					linha = x / n;
+					coluna = x % n - 1;
+				} else {
+					linha = x / n;
+					coluna = x % n - 1;
 				}
 			}
-			System.out.println("numero aleatorio gerado: "+ x);
-			System.out.println("posicao correspondente do "+classe+" na matriz: ["+linha+"]["+coluna+"]");				
+			System.out.println("numero aleatorio gerado: " + x);
+			System.out.println("posicao correspondente do " + classe + " na matriz: [" + linha + "][" + coluna + "]");
 		}
 	}
-	
+
 	public void conectaNether(Nether neth) {
 		this.neth = neth;
 	}
--- trabalho/src/pacote/Sala.java
+++ trabalho/src/pacote/Sala.java
@@ -10,17 +10,20 @@ public class Sala {
 		atores = new ArrayList<Ator>();
 		int x, y, z;
 		Random rand = new Random();
-		x = rand.nextInt(101)+1;
+		
+		x = rand.nextInt(100)+1;
 		if(x > 90){ // 10% de chance de criar um monstro junto com a sala
 			Monstro monstro = new Monstro();
 			atores.add(monstro);
 		}
-		y = rand.nextInt(101)+1;
+		
+		y = rand.nextInt(100)+1;
 		if(y > 90){ // 10% de chance de criar um kitMedico junto com a sala
 			KitMedico kit = new KitMedico();
 			atores.add(kit);
 		}
-		z = rand.nextInt(101)+1;
+		
+		z = rand.nextInt(100)+1;
 		if(y > 90){ // 10% de chance de criar uma municao junto com a sala
 			Municao municao = new Municao();
 			atores.add(municao);
