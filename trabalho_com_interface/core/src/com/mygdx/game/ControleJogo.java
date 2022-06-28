7e590a5ebd2d1f5a0ec6670e1a399bde94e31a76
T-GAMER <T-GAMER@DESKTOP-CHET23P.mshome.net>
2022-06-28 00:41:11 -0300

    Plano de exceções feito

--- trabalho_com_interface/core/src/com/mygdx/game/ControleJogo.java
+++ trabalho_com_interface/core/src/com/mygdx/game/ControleJogo.java
@@ -15,6 +15,7 @@ public class ControleJogo {
     ArrayList<String> mensagens;
 
 	public void conectaAtor(Sala s[][], String tipo) {
+		
 		for(int i = 0; i < 5; i++) {
 			for(int j = 0; j < 5; j++) {
 				for(int k = 0; k < s[i][j].atores.size(); k++) {
